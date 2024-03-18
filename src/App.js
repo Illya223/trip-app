5cb4c7a146ca3736234abde5d58d9e96e158c2da
Illya Kyrychenko <kyrychenko.illya@gmail.com>
2024-03-18 18:44:23 +0200

    API Gmail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
 import Main from './Pages/Main';
-
+import { useEffect } from 'react';
 
 
 function App() {
+
+  const google = window.google;
+
+  const handleCallbackResponse = (response) => {
+    console.log("Encoded JWT ID token: " + response.credential)
+
+  }
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id: "1007978692269-ibkqmt2j6u7qkucitno2gr9n31fpljnk.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("sign-in-div"),
+      { theme: "outline", size: "large"}
+    )
+  }, [])
+
   return (
     <div class="app">
+
       <Main></Main>
-      
+      <div id="sign-in-div"></div>
     </div>
   );
 }
